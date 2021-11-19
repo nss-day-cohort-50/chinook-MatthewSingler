50caa62cf7e1f9d65ae2ba8efced8c1b332174f4
Matthew Singler <matthew.singler@gmail.com>
2021-11-19 15:24:37 -0600

    total line items for each Invoice Id

--- line_items_per_invoice.sql
+++ line_items_per_invoice.sql
@@ -1,11 +1,4 @@
-SELECT COUNT(i.InvoiceLineId)
-    i.InvoiceLineId,
+SELECT COUNT(i.InvoiceLineId),
     i.InvoiceId
 FROM InvoiceLine i
-JOIN Invoice Inv
-ON Inv.id = i.InvoiceId
-GROUP BY Inv.id
-
-SELECT COUNT(InvoiceId), InvoiceLineId 
-FROM InvoiceLine 
-GROUP BY InvoiceId;
+GROUP BY i.InvoiceId
