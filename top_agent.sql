a3fd2fd1d0c2f1b7005c7be99a9999404c05cb7d
Matthew Singler <matthew.singler@gmail.com>
2021-11-23 10:04:14 -0600

    top sales agent in sales INCOMPLETE

---
+++ top_agent.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(i.Total),
+    (e.FirstName || ' ' || e.LastName) EmpName
+FROM Employee e
+JOIN Customer c
+ON e.EmployeeId = c.SupportRepId
+JOIN Invoice i
+ON c.CustomerId = i.CustomerId
